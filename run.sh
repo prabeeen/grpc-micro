006e9e11642fa903b6836dc2e091109d1c09d377
prabin <prabin.171342@ncit.edu.np>
2024-04-07 22:55:08 +0545

    feat: added bash script and actions

---
+++ run.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+SERVICE_NAME=$1
+RELEASE_VERSION=$2
+
+sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+protoc --go_out=./golang --go_opt=paths=source_relative \
+    --go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
+    ./${SERVICE_NAME}/*.proto
+cd golang/${SERVICE_NAME}
+go mod init \
+    github.com/prabeeen/grpc-micro/golang/${SERVICE_NAME} || true
+go mod tidy
+cd ../../
+git config --global user.email "prabin.171342@ncit.edu.np"
+git config --global user.name "prabin"
+git add . && git commit -am "proto update" || true
+git tag -fa golang/${SERVICE_NAME}/${RELEASE_VERSION} \
+    -m "golang/${SERVICE_NAME}/${RELEASE_VERSION}"
+git push origin refs/tags/golang/${SERVICE_NAME}/${RELEASE_VERSION}
